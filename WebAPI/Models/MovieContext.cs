95ee7671db175ccd3255c0976c37aa080c4b4214
Ogulc <Ogulc@DESKTOP-AVHLJJN>
2023-07-09 22:29:35 +0300

    Add project files.

---
+++ WebAPI/Models/MovieContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Movie> Movies { get; set; } = null!;
+    }
+}
